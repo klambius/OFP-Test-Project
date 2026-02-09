020a2c5f6b1f328459f60811e2233be04deeecdb
klambius <denis.dynkin@yandex.ru>
2026-02-09 19:42:04 +0300

    Добавил conftest.py

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def initialize_web_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized --disable-extensions")
+
+    # Получаем папку драйвера
+    driver_folder = os.path.dirname(ChromeDriverManager().install())
+    # Полный путь к chromedriver.exe
+    driver_path = os.path.join(driver_folder, "chromedriver.exe")
+
+    service = Service(driver_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    yield driver
+    driver.quit()
