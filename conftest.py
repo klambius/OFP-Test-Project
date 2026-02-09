fc4700261b416f5b737e332302945c74ff301847
klambius <denis.dynkin@yandex.ru>
2026-02-09 22:52:00 +0300

    Добавил опции в браузер для CI прогонов

--- conftest.py
+++ conftest.py
@@ -8,6 +8,12 @@ def driver():
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-popup-blocking")
+    chrome_options.add_argument(
+        "--disable-blink-features=AutomationControlled")
 
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
